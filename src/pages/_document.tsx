d81566ef30a100be354ffb32b2c771dcd73661e5
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-14 19:32:34 +0200

    [draft] remove google bots essential

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="google" content="notranslate" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
