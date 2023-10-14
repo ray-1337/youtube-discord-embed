96d9eed94d581844f5110b0746996311659d7c53
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-14 19:38:18 +0200

    add csp just for safety measurement

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+
+  async headers() {
+    const scriptCSP = ['script-src', "'self'"];
+
+    if (process.env.NODE_ENV === "development") {
+      scriptCSP.push("'unsafe-eval'");
+    };
+
+    return [{
+      source: "/:path",
+      headers: [{
+        key: "Content-Security-Policy",
+        value: [
+          ['default-src', "'self'"],
+          scriptCSP,
+          ['style-src', "'self'"],
+          ['connect-src', "'self'"],
+          ['img-src', "'self'"],
+          ['base-uri',  "'self'"],
+          ['form-action', "'self'"],
+          ['font-src', "'self'"],
+          ['object-src', "'none'"],
+          ['frame-ancestors', "'none'"],
+          ['block-all-mixed-content'],
+          ['upgrade-insecure-requests']
+        ]
+        .reduce((prev, [directive, ...policy]) => {
+          return `${prev}${directive} ${policy.filter(Boolean).join(' ')};`
+        }, '')
+      }]
+    }]
+  }
+};
 
 module.exports = nextConfig
