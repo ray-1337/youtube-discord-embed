f8dbb80358c0f9bb70668fa03b27d5cc7eb20944
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-10-15 08:07:52 +0200

    add a function to dynamically get youtube ID from URL

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,7 @@
+import type { GetServerSidePropsContext } from "next";
+
 type HeightAndWidth = "height" | "width";
 
 export type YouTubeMetadataBeforeDOM = Partial<Record<"host" | "thumbnail_url" | "url" | "title" | `author_${"name" | "url"}`, string> & Record<HeightAndWidth, number>>;
+
+export type ServerSidePropsWithV = GetServerSidePropsContext<{v?: string}>;
