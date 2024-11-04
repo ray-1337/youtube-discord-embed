d727f467074bb96ac57669e349cb9a03815412ce
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-05 04:47:25 +0800

    revert oembed changes (my bad)

--- src/pages/[ytID].tsx
+++ src/pages/[ytID].tsx
@@ -73,19 +73,7 @@ const WatchPage: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (p
             <meta name="twitter:player:stream" content={props?.video_url} />
             <meta name="twitter:player:stream:content_type" content={"video/mp4"} />
 
-            <link rel="alternate" type="application/json+oembed" title={authorText}>
-              {
-                JSON.stringify({
-                  "author_name": "YouTube Discord Embed, inspired by FixTwitter.",
-                  "author_url": "https://13373333.one",
-                  "provider_name": authorText,
-                  "provider_url": defaultFallbackValueURL,
-                  "title": "YouTube Discord Embed, inspired by FixTwitter.",
-                  "type": "link",
-                  "version": "1.0"
-                })
-              }
-            </link>
+            <link rel="alternate" href={`https://${props?.host}/api/oembed?text=${props?.title}&url=${fallbackURL}`} type="application/json+oembed" title={authorText} />
           </Fragment>
         )
       }
@@ -138,7 +126,8 @@ export async function getServerSideProps(ctx: ServerSidePropsWithV) {
       video_url: firstRawVideoURL.url,
       url: `https://youtu.be/${youtubeID}`,
       height: firstRawVideoURL.height,
-      width: firstRawVideoURL.width
+      width: firstRawVideoURL.width,
+      host: ctx?.req?.headers?.host
     };
 
     cache.set(youtubeID, content);
---
+++ src/pages/api/oembed.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export const defaultEmbed = {
+  "author_name": "YouTube Discord Embed, inspired by FixTwitter.",
+  "author_url": "https://13373333.one",
+  "provider_name": "YouTube / 13373333.one",
+  "provider_url": "https://github.com/ray-1337/youtube-discord-embed",
+  "title": "YouTube Discord Embed, inspired by FixTwitter.",
+  "type": "link",
+  "version": "1.0"
+};
+
+const safelySlice = (str: string) => str.slice(0, 256);
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  let forwardedEmbed = defaultEmbed;
+
+  if (req?.query?.text?.length) {
+    forwardedEmbed.author_name = safelySlice(req.query.text as string);
+    forwardedEmbed.title = safelySlice(req.query.text as string);
+  };
+
+  if (req?.query?.url?.length) {
+    forwardedEmbed.author_url = safelySlice(req.query.url as string);
+  };
+
+  res.status(200).json(forwardedEmbed);
+};
