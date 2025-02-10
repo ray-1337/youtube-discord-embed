fb9f2e26af64a2ad0d4ee3d7f116dec77942ec75
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-02-11 03:37:05 +0800

    update @distube/ytdl-core version to v4.16.3

--- src/helpers/utility.ts
+++ src/helpers/utility.ts
@@ -2,11 +2,13 @@ import { validateID } from "@distube/ytdl-core";
 import { type ParsedUrlQuery } from "node:querystring";
 
 export function dynamicSearchForYouTubeID(query?: ParsedUrlQuery) {
-  if (query?.ytID === "watch" && query?.v?.length && validateID(query.v as string)) {
-    return String(query.v);
-  } else if (validateID(query?.ytID as string)) {
-    return String(query?.ytID);
-  };
+  try {
+    if (query?.ytID === "watch" && query?.v?.length && validateID(query.v as string)) {
+      return String(query.v);
+    } else if (validateID(query?.ytID as string)) {
+      return String(query?.ytID);
+    };
+  } catch {};
 
   return null;
 };
